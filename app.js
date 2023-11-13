6197c68bbe725c629089a471ff1c1a32d94410f9
hirohiko <hirohiko.work@gmail.com>
2023-11-13 14:27:55 +0900

    style: Improved code readability

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-// express => WebServer Build
+// WebServer Setting(Express)
 const express = require("express");
 const app = express();
 const port = 3000;
 
-//  access to mysql
+//  Database Setting(MySQL)
 const mysql = require("mysql2");
-
 const con = mysql.createConnection({
   host: "localhost",
   user: "nodejsuser",
@@ -13,14 +12,21 @@ const con = mysql.createConnection({
   database: "learning2023",
 });
 
-con.connect();
-con.query("SELECT * FROM customer", function (error, results, fields) {
-  if (error) throw error;
-  console.log(results[0]);
-  con.end();
+// Database-On
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected");
 });
-
-app.get("/", (req, res) => res.send("Hello World."));
-
-// Server On
+// Server-On
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// Request & Response
+app.get("/", (req, res) => {
+  const sqlTest = "SELECT * FROM customer";
+  con.query(sqlTest, function (error, results, fields) {
+    if (error) throw error;
+    console.log(results[0]);
+    con.end();
+  });
+  res.send("Hello World.");
+});
